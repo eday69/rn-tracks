eafa6a48ddd26c155ee78699ca829db0934a3b49
Eric Day <eric.ch.day@gmail.com>
2023-11-28 19:05:10 -0700

    sec 15 part V

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,8 +1,42 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import '../_mockLocation';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { requestForegroundPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
+import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
-  return <Text style={{ fontSize: 48 }}>TrackCreateScreen</Text>
+  const [error, setError] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestForegroundPermissionsAsync();
+      await watchPositionAsync({
+        accuracy: Accuracy.BestForNavigation,
+        timeInterval: 1000,
+        distanceInterval: 10
+      }, (location) => {
+        console.log(location);
+      });
+      if (!granted) {
+        throw new Error('Location permission not granted');
+      }
+    } catch (err) {
+      setError(err);
+    }
+  }
+  useEffect(() => {
+    startWatching();
+  }, []);
+
+  return (
+    <SafeAreaView forceInset={{ top: 'always' }}>
+      <Text h2>Create a Track</Text>
+      <Map />
+      { error ? <Text>Please enable location services</Text> : null }
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({});
